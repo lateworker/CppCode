436451f13a11cc049eb25c1087b33dbf35f53046
lateworker <lateworker@qq.com>
2025-05-06 22:04:27 +0800

    tuesday

--- P3809 【模板】后缀排序.cpp
+++ P3809 【模板】后缀排序.cpp
@@ -2,22 +2,21 @@
 using namespace std;
 vector<int> sa, rk, ht;
 int gSA(const string &s, int n) {
-	int m = 256; vector<int> id(n * 2 + 1), _rk(n * 2 + 1);
+	int m = 256; vector<int> _sa(n * 2 + 1), _rk(n * 2 + 1);
 	sa.resize(n * 2 + 1, 0), rk.resize(n * 2 + 1, 0), ht.resize(n + 1, 0);
 	auto ctsort = [&]() { vector<int> cnt(m + 1);
 		for (int i = 1; i <= n; i++) cnt[rk[i]]++;
 		for (int i = 1; i <= m; i++) cnt[i] += cnt[i - 1];
-		for (int i = n; i >= 1; i--) sa[cnt[rk[id[i]]]--] = id[i];
-	}; for (int i = 1; i <= n; i++) { rk[i] = s[i], id[i] = i; } ctsort();
+		for (int i = n; i >= 1; i--) sa[cnt[rk[_sa[i]]]--] = _sa[i];
+	}; for (int i = 1; i <= n; i++) { rk[i] = s[i], _sa[i] = i; } ctsort();
 	for (int w = 1, p; p = 0, 1; w <<= 1, m = p) {
-		for (int i = n - w + 1; i <= n; i++) id[++p] = i;
-		for (int i = 1; i <= n; i++) if (sa[i] > w) id[++p] = sa[i] - w;
+		for (int i = n - w + 1; i <= n; i++) _sa[++p] = i;
+		for (int i = 1; i <= n; i++) if (sa[i] > w) _sa[++p] = sa[i] - w;
 		ctsort(); _rk.swap(rk), p = 0;
 		for (int i = 1; i <= n; i++)
 			rk[sa[i]] = (_rk[sa[i]] == _rk[sa[i - 1]] && _rk[sa[i] + w] == _rk[sa[i - 1] + w]) ? p : ++p;
 		if (p == n) break;
-	}
-	for (int i = 1, k = 0; i <= n; i++)
+	} for (int i = 1, k = 0; i <= n; i++)
 		if (rk[i]) { for (k -= (k > 0); s[i + k] == s[sa[rk[i] - 1] + k]; k++); ht[rk[i]] = k; }
 	sa.resize(n + 1), rk.resize(n + 1); return n;
 }
