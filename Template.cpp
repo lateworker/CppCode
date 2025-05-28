cbac379348de5793968b49d6f70a7d4f839e71cc
lateworker <lateworker@qq.com>
2025-05-28 17:34:09 +0800

    Template

---
+++ Template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<class T_, size_t N_, T_ DEFAULT = T_()>
+class Array {
+	T_ val[N_]; size_t ver[N_], clk;
+public:
+	Array() { clk = 1; }
+	T_& operator[] (size_t i) { if (ver[i] != clk) ver[i] = clk, val[i] = DEFAULT; return val[i]; }
+	void clear() { ++clk; }
+};
+
+template<class T_, size_t N_>
+class Graph{
+	inline static size_t psz;
+	inline static pair<T_, size_t> pool[N_];
+	struct iterator {
+		size_t now;
+		T_& operator* () const { return pool[now].first; }
+		bool operator== (iterator it) const { return now == it.now; }
+		iterator& operator++ () { now = pool[now].second; return *this; }
+	}; size_t head;
+public:
+	Graph() { head = 0; }
+	iterator begin() const { return {head}; }
+	iterator end() const { return {0}; }
+	void push_front(T_ val) { ++psz, this->pool[psz] = {val, head}, head = psz; }
+};
+
+int main() {
+	return 0;
+}
