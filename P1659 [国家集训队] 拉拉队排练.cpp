39ecbeec9e425b491da61ecdf2ec42fec6a2b34e
lateworker <lateworker@qq.com>
2025-06-20 21:57:25 +0800

    ac

--- P1659 [国家集训队] 拉拉队排练.cpp
+++ P1659 [国家集训队] 拉拉队排练.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 using intl = long long;
 const intl N = 1000000, Mod = 19930726;
+intl fpow(intl a, intl b) { intl c = 1; for (; b; (a *= a) %= Mod, b >>= 1) if (b & 1) (c *= a) %= Mod; return c; }
 struct Pam {
 	vector<int> ch;
-	int fa, len, cnt;
+	int fa; intl len, cnt;
 } st[N + 10];
 int last, psz;
-int n, m;
+int n; intl m;
 char s[N + 10];
 int node(int len, int fa) { return st[++psz] = {vector<int>(26, 0), fa, len, 0}, psz; }
 void init() { s[0] = '#', st[0] = {vector<int>(26, 0), 1, 0, 0}, st[psz = 1] = {vector<int>(26, 0), 1, -1, 0}; }
 int shorten(int u, int i) { while (s[i] != s[i - st[u].len - 1]) u = st[u].fa; return u; }
-int main() {
+int main() { cin.tie(0)->sync_with_stdio(0);
 	cin >> n >> m; init();
 	for (int i = 1; i <= n; i++) {
 		cin >> s[i];
@@ -21,8 +22,8 @@ int main() {
 		last = st[p].ch[c], ++st[last].cnt;
 	}
 	for (int i = psz; ~i; i--) st[st[i].fa].cnt += st[i].cnt;
-	vector<pair<int, int>> vec;
-	int sum = 0;
+	vector<pair<intl, intl>> vec;
+	intl sum = 0;
 	for (int i = 2; i <= psz; i++) {
 		if (~ st[i].len & 1) continue;
 		vec.push_back({st[i].len, st[i].cnt});
@@ -32,7 +33,7 @@ int main() {
 	sort(vec.begin(), vec.end(), [&](const auto& u, const auto& v) { return u.first > v.first; } );
 	intl ans = 1;
 	for (auto [len, cnt] : vec) {
-		for (int i = 1; i <= min(cnt, m); i++) (ans *= len) %= Mod;
+		(ans *= fpow(len, min(intl(cnt), m))) %= Mod;
 		m -= cnt; if (m <= 0) break;
 	}
 	cout << ans << "\n";
