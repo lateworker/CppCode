fb4dc08a6c02245ab94b579560efca8a98e6f5ea
lateworker <lateworker@qq.com>
2025-06-28 22:18:31 +0800

    高精度

--- test.cpp
+++ test.cpp
@@ -1,12 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, b;
 int main() {
-	int* c;
-	c = a + ++ b;
-	c = a + ++ b;
-	c = a + ++ b;
-	c = a +++ b;
-	cout << *c << "\n";
+	int x; cin >> x; cout << (+x) << "\n";
 	return 0;
 }
---
+++ test01.py
@@ -0,0 +1,6 @@
+bina = int(input(), 2)
+deca = int(input())
+print(bina == deca)
+if bina != deca:
+    print(bina)
+    print(bin(deca))
