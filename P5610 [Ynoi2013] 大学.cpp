f09b6fbec3ec2edb24803b65445cd0d99d27ac6e
lateworker <lateworker@qq.com>
2025-08-20 23:04:08 +0800

    practice

--- P5610 [Ynoi2013] 大学.cpp
+++ P5610 [Ynoi2013] 大学.cpp
@@ -1,62 +1,63 @@
 #include <bits/stdc++.h>
+#define chkmax(x, y) x = std::max(x, y)
+#define chkmin(x, y) x = std::min(x, y)
 using namespace std;
+const int SIZE = 1 << 20; char buf[SIZE], *p1, *p2;
+#define gc() (p1==p2&&(p2=(p1=buf)+fread(buf,1,SIZE,stdin),p1==p2)?EOF:*p1++)
+template<typename T> inline T& rd(T &x) {
+	x = 0; int f = 1; char c = gc();
+	for (; !isdigit(c); c = gc())if (c == '-')f = -1;
+	for (; isdigit(c); c = gc())x = x * 10 + (c ^ 48);
+	return x *= f;
+}
 using intl = long long;
-const int N = 100000, A = 500000;
-struct Treap { int lson, rson, idx, pri; } st[N * int(sqrt(A)) + 10]; int psz;
-#define le(u) st[u].lson
-#define ri(u) st[u].rson
-int node(int idx) { st[++psz] = {0, 0, idx, rand()}; return psz; }
-void split(int u, int idx, int& L, int& R) {
-	if (u == 0) return L = R = 0, void();
-	if (st[u].idx <= idx) L = u, split(ri(u), idx, ri(u), R);
-	else R = u, split(le(u), idx, L, le(u));
-}
-int merge(int u, int v) {
-	if (u == 0 || v == 0) return u + v;
-	if (st[u].pri > st[v].pri) { ri(u) = merge(ri(u), v); return u; }
-	else { le(v) = merge(u, le(v)); return v; }
-}
-int n, m, rt[A + 10];
-intl a[N + 10], b[N + 10];
-void tnumUpdate(int u, intl val) { for (; u <= n; u += u & -u) b[u] += val; }
-intl tnumQuery(int u) { intl res = 0; for (; u; u -= u & -u) res += b[u]; return res; }
-int dfs(int u, int x) {
-	if (u == 0) return 0;
-	int i = st[u].idx, L = le(u), R = ri(u), now = 0;
-	if (a[i] % x == 0) {
-		tnumUpdate(i, - a[i]);
-		tnumUpdate(i, a[i] /= x);
-		if (a[i] > 1) now = u, le(u) = ri(u) = 0;
-	} return merge(merge(dfs(L, x), now), dfs(R, x));
-}
+const int N = 100000, A = 500000, RTA = int(sqrt(A)) + 1;
+int n, m, a[N + 10], cnt[A + 10], psz[A + 10], c[N + 10], maxa;
+int idx[N * RTA + 10], fa[N * RTA + 10], tn[A + 10], t[N + 10][RTA + 10];
+inline void node(const int& r, const int& i) { idx[psz[r]] = i, fa[psz[r]] = psz[r], ++psz[r]; }
+inline int find(const int& r, const int& u) { return u == tn[r + 1] || u == fa[u] ? u : fa[u] = find(r, fa[u]); }
+inline void del(const int& u) { fa[u] = u + 1; }
+intl b[N + 10];
+inline void tnumUpdate(int u, const intl& val) { for (; u <= n; u += u & -u) b[u] += val; }
+inline intl tnumQuery(int u) { intl res = 0; for (; u; u -= u & -u) res += b[u]; return res; }
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin >> n >> m;
-	for (int i = 1; i <= n; i++) {
-		cin >> a[i]; tnumUpdate(i, a[i]);
-		for (intl x = 2, y; x * x <= a[i]; x++) {
-			if (a[i] % x) continue;
-			rt[x] = merge(rt[x], node(i));
-			if (x * x == a[i]) continue;
-			y = a[i] / x;
-			rt[y] = merge(rt[y], node(i));
-		} rt[a[i]] = merge(rt[a[i]], node(i));
+	rd(n), rd(m);
+	for (int i = 1; i <= n; ++i) {
+		rd(a[i]);
+		tnumUpdate(i, a[i]);
+		++cnt[a[i]];
+		chkmax(maxa, a[i]);
+	}
+	for (int i = 1; i <= maxa; ++i) cnt[i] += cnt[i - 1], psz[i] = cnt[i - 1];
+	for (int i = 1; i <= n; ++i) c[++psz[a[i]]] = i;
+	psz[2] = tn[2] = 1;
+	for (int x = 2; x <= maxa; ++x) {
+		for (int y = x; y <= maxa; y += x) tn[x + 1] += cnt[y] - cnt[y - 1];
+		psz[x + 1] = (tn[x + 1] += tn[x]);
+	}
+	static int vec[N + 10];
+	for (int x = 2; x <= maxa; ++x) {
+		int vec0 = 0;
+		for (int y = x; y <= maxa; y += x)
+			for (int i = cnt[y - 1] + 1; i <= cnt[y]; ++i) vec[++vec0] = c[i];
+		stable_sort(vec + 1, vec + vec0 + 1);
+		for (int i = 1; i <= vec0; ++i) node(x, vec[i]);
 	}
 	intl last = 0;
-	for (int i = 1; i <= m; i++) {
-		int op, l, r;
-		cin >> op >> l >> r;
+	for (int i = 1; i <= m; ++i) {
+		intl op, l, r;
+		rd(op), rd(l), rd(r);
 		l ^= last, r ^= last;
 		if (op == 1) {
-			intl x; cin >> x; x ^= last;
-			if (x == 1) continue;
-			int L, now, R;
-			split(rt[x], r, L, R), split(L, l - 1, L, now);
-			now = dfs(now, x);
-			rt[x] = merge(merge(L, now), R);
-		} else if (op == 2) {
-			cout << (last = tnumQuery(r) - tnumQuery(l - 1)) << "\n";
-		}
+			intl x; rd(x); x ^= last;
+			if (x == 1 || tn[x] == tn[x + 1]) continue;
+			int p = lower_bound(idx + tn[x], idx + tn[x + 1], l) - idx;
+			for (p = find(x, p); p < tn[x + 1] && idx[p] <= r; p = find(x, p + 1)) {
+				int j = idx[p];
+				if (a[j] % x) del(p);
+				else tnumUpdate(j, - a[j] + a[j] / x), a[j] /= x;
+			}
+		} else printf("%lld\n", last = tnumQuery(r) - tnumQuery(l - 1));
 	}
 	return 0;
 }
---
+++ P9871_3.cpp
@@ -0,0 +1,80 @@
+#define ffopen(s) \
+cin.tie(0)->sync_with_stdio(0); \
+if (*#s) freopen(#s ".in", "r", stdin); \
+if (*#s) freopen(#s ".out", "w", stdout); \
+/**/
+#include <bits/stdc++.h>
+#define int intl
+#define chkmax(x, y) (x = std::max(x, y))
+#define chkmin(x, y) (x = std::min(x, y))
+using namespace std;
+using intl = long long;
+const intl M = 200000, inf = 0x3f3f3f3f3f3f3f3f;
+int n, m, k;
+struct Pair { int l, r, v; bool operator< (const Pair& b) const { return r < b.r; } } qry[M + 10];
+intl st[M * 2 + 10], tg[M * 2 + 10], d;
+void pushup(int u) { if (u) st[u] = max(st[u << 1], st[u << 1 | 1]); }
+void modify(int u, intl val) { st[u] += val, tg[u] += val; }
+void pushdown(int u) { if (tg[u]) modify(u << 1, tg[u]), modify(u << 1 | 1, tg[u]), tg[u] = 0; }
+void updateAdd(int l, int r, intl val) {
+	l += n, r += n + 1;
+	for (int i = __lg(n) + 1; i; i--) pushdown(l >> i), pushdown(r >> i);
+	for (int u = 0, v = 0; l < r; l >>= 1, r >>= 1) {
+		if (l & 1) u = l, modify(l++, val);
+		if (r & 1) v = r, modify(--r, val);
+		do pushup(u >>= 1); while (l == r && u);
+		do pushup(v >>= 1); while (l == r && v);
+	}
+}
+void modifyMax(int u, intl val) {
+	u += n;
+	for (int i = __lg(n) + 1; i; i--) pushdown(u >> i), chkmax(st[u >> i], val);
+	chkmax(st[u], val);
+}
+intl queryMax(int l, int r) {
+//	if (l > r) return -inf;
+	assert(l <= r);
+	l += n, r += n + 1;
+	for (int i = __lg(n) + 1; i; i--) pushdown(l >> i), pushdown(r >> i);
+	intl res = -inf;
+	for (; l < r; l >>= 1, r >>= 1) {
+		if (l & 1) chkmax(res, st[l++]);
+		if (r & 1) chkmax(res, st[--r]);
+	} return res;
+}
+vector<int> vec;
+int lower(int x) { return lower_bound(vec.begin(), vec.end(), x) - vec.begin(); }
+void slove() {
+	cin >> n >> m >> k >> d;
+	for (int i = 1; i <= m; i++) {
+		int x, y, z;
+		cin >> x >> y >> z;
+		if (y > k) continue;
+		qry[i] = {x - y, x, z};
+		vec.push_back(qry[i].l), vec.push_back(qry[i].r);
+	}
+	for (int i = 1; i <= n; i++) vec.push_back(i);
+	sort(qry + 1, qry + m + 1);
+	sort(vec.begin(), vec.end());
+	vec.erase(unique(vec.begin(), vec.end()), vec.end());
+	n = vec.size() - 1;
+	fill(tg + 1, tg + (n << 1) + 1, 0);
+	for (int i = 1; i <= n; i++) st[i + n] = d * i;
+	for (int u = n - 1; u; u--) pushup(u);
+	intl f = 0, ptr = 1;
+	for (int ii = 1; ii <= n; ii++) {
+		const int& i = vec[ii]; assert(ii == i);
+		modifyMax(ii, f + (intl) d * i);
+		for (; ptr <= m && qry[ptr].r == i; ++ptr) updateAdd(0, lower(qry[ptr].l), qry[ptr].v);
+		chkmax(f, queryMax(lower(i - k), ii - 1) - (intl) d * i);
+	}
+	cout << f << "\n";
+}
+signed main() { ffopen(run/run3);
+	int C, T; cin >> C >> T;
+	while (T--) {
+		vec = {0};
+		slove();
+	}
+	return 0;
+}
